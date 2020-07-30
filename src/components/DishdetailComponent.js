97e2c30420445d3a982d3188e422801f71e60d6b
Rahman <dolapobadru@gmail.com>
2020-07-31 00:42:40 +0100

    Redux thunk and redux logger

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Card , CardBody, CardTitle , CardText , CardImg , Breadcrumb , BreadcrumbItem, Button , Modal , ModalHeader , ModalBody , Col , Row , Label} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control , LocalForm , Errors} from 'react-redux-form'
+import {Loading} from './LoadingComponent';
 
 
 
@@ -55,7 +56,26 @@ import {Control , LocalForm , Errors} from 'react-redux-form'
     }
 
     const Dishdetail = (props) => {
-        const dish =props.dish;
+         if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {}
+        const dish = props.dish;
         if(dish == null){
             return(
                 <div></div>
